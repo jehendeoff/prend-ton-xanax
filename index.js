892e2252bb1cf03d5467a5c48f4f3a89ee2731ee
Jéhèndé <jehende@jehende.fr>
2022-06-24 18:11:52 +0200

    now, it works on itself (no opera)

--- index.js
+++ index.js
@@ -10,6 +10,13 @@ if(!fs.existsSync("./config.yml")){
 global.config = yaml.parse(fs.readFileSync("./config.yml", "utf-8"));
 
 if (!fs.existsSync(global.config.animePath)) throw new Error("AnimePath is unavailable.");
+if (!global.config.puppeteer?.chromePath){
+	if (!global.config.puppeteer) global.config.puppeteer = {};
+	const localChromeium =  __dirname+"/node_modules/puppeteer/.local-chromium/";
+	const version = fs.readdirSync(localChromeium)[0] + "/";
+	const os = fs.readdirSync(localChromeium + version)[0] + "/";
+	global.config.puppeteer.chromePath =localChromeium + version + os + "chrome";
+} 
 if (!fs.existsSync(__dirname + "/cookies/")) fs.mkdirSync(__dirname + "/cookies/");
 app.listen(global.config.app.port ?? 10410);
 app.SetDownloader(downloader);
