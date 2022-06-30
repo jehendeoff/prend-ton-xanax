ac8f25a7536ae8b25740f837a5b766abc4cb7230
Jéhèndé <jehende@jehende.fr>
2022-06-30 13:55:41 +0200

    shloud fix error

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 
+require("./config parser");
 const downloader = require("./downloader");
 const tracer = require("./tracer");
 const app = require("./app");
-require("./config parser");
 app.listen(global.config.app.port ?? 10410);
 app.SetDownloader(downloader);
 app.SetTracer(tracer);
