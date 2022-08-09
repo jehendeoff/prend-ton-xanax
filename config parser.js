5649579af2c897a2dc0c59b8d17d50c5cef3b367
Jehende <45318464+jehendeoff@users.noreply.github.com>
2022-08-09 20:15:27 +0200

    Update config parser.js

--- config parser.js
+++ config parser.js
@@ -7,7 +7,7 @@ if(!fs.existsSync(configPath)){
 global.config = yaml.parse(fs.readFileSync("./config.yml", "utf-8"));
 
 let updated = false;
-//ANCHOR update old config 
+//ANCHOR update old config
 if (global.config.animePath !== undefined
 && global.config.anime.path === undefined){
 	console.warn("Found old config `animePath`, replacing it with `anime.path`.\t(config.yml => anime.path)");
@@ -23,7 +23,7 @@ if (global.config.anime.path === undefined
 || global.config.anime.path === null){
 	global.config.anime.path = __dirname + "/anime/";
 	console.warn("Did not found any anime path in config, making one.\t(config.yml => anime.path)");
-	if (!fs.existsSync(global.config.anime.path)) 
+	if (!fs.existsSync(global.config.anime.path))
 		fs.mkdirSync(global.config.anime.path);
 	updated = true;
 }
@@ -39,7 +39,7 @@ if (global.config.app.cookie === undefined
 || global.config.app.cookie === null){
 	global.config.app.cookie = __dirname + "/cookie/";
 	console.warn("Did not found any app cookie path in config, making one.\t(config.yml => app.cookie)");
-	if (!fs.existsSync(global.config.app.cookie)) 
+	if (!fs.existsSync(global.config.app.cookie))
 		fs.mkdirSync(global.config.app.cookie);
 	updated = true;
 }
@@ -62,8 +62,8 @@ if (!global.config.puppeteer?.chromePath){
 	console.warn("did not found any chrome executable ine the config file, trying to default to one.\t(config.yml => puppeteer => chromePath)");
 	if (!global.config.puppeteer) global.config.puppeteer = {};
 	const localChromeium =  __dirname+"/node_modules/puppeteer/.local-chromium/";
-	const version = fs.readdirSync(localChromeium).filter(file => 
-		fs.statSync(localChromeium+file).isDirectory() 
+	const version = fs.readdirSync(localChromeium).filter(file =>
+		fs.statSync(localChromeium+file).isDirectory()
 		&& fs.readdirSync(localChromeium+file).filter(folder => folder.includes("chrome")).length >0
 	)[0] + "/";
 	const os = fs.readdirSync(localChromeium + version).filter(file => fs.statSync(localChromeium+version+file).isDirectory())[0] + "/";
@@ -74,10 +74,10 @@ if (!global.config.puppeteer?.chromePath){
 		chromeExec = chromeExec.sort((a,b)=>{
 			const aExe = a.endsWith(".exe");
 			const bExe = b.endsWith(".exe");
-			
-			if (aExe === true 
+
+			if (aExe === true
 			&& bExe === true) return 0;
-			
+
 			if (aExe === true) return -1;
 			if (bExe === true) return 1;
 			return 0;
@@ -91,7 +91,7 @@ if (!global.config.puppeteer?.chromePath){
 	}
 	global.config.puppeteer.chromePath =localChromeium + version + os + chromeExec[0];
 	updated = true;
-} 
+}
 
 
 if (updated === true) fs.writeFileSync(configPath, yaml.stringify(global.config));
